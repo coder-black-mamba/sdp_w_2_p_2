af2f4c4d870ef1cba1928dba14511d1ecd518146
coder-black-mamba <it.is.the.black.mamba@gmail.com>
2025-06-05 13:08:19 +0600

    ui enhace

---
+++ main.js
@@ -0,0 +1,62 @@
+const ITEM_DETAILS=`
+<!-- item details starts -->
+        <div class="col-md-12" id="item-details">
+          <div class="card m-2 p-2">
+            <img class="card-img-top rounded w-100 object-fit-cover" width="200px" height="300"  src="https://www.themealdb.com/images/media/meals/c9a3l31593261890.jpg" alt="Card image cap">
+            <div class="card-block">
+              <h4 class="card-title py-2">Item Name</h4>
+              <p class="card-text text-muted">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <ul>
+                <li>Item Name : quantity</li>
+                <li>Item Name : quantity</li>
+                <li>Item Name : quantity</li>
+                <li>Item Name : quantity</li>
+                <li>Item Name : quantity</li>
+              </ul>
+              <a href="#" class=" d-block w-100 my-2">See source</a>
+            </div>
+          </div>
+        </div>
+<!-- item details ends -->
+`
+const SINGLE_ITEM_CARDS=`
+<!-- single item card starts-->
+        <div class="col-md-4">
+          <div class="card m-2 p-2">
+            <img class="card-img-top rounded" src="https://www.themealdb.com/images/media/meals/c9a3l31593261890.jpg" alt="Card image cap">
+            <div class="card-block">
+              <h4 class="card-title py-2">Card title</h4>
+              <p class="card-text text-muted">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <button type="button" class="btn btn-outline-warning d-block w-100 my-2">See Details </button>
+            </div>
+          </div>
+        </div>
+ <!-- single item card ends-->
+
+`
+
+const NOT_FOUND=`
+     <!-- not found -->
+      <div class="col-md-12" id="not-found">
+        <div class="bg-danger text-white p-3 rounded">
+          <h2 class="text-center">Could Not Found Any Item !</h2>
+          <p class="text-center">Please Try Again !</p>
+          <p class="text-center">ভ্রুম ভ্রুম ভ্রুম 😛😛😛</p>
+        </div>
+      </div>
+      <!-- not found ends -->
+`   
+
+const LOADING = `
+      <!-- loading -->
+      <div class="col-md-12" id="loading">
+        <div class="bg-warning text-white p-3 rounded">
+          <h2 class="text-center">Loading...</h2>
+          <p class="text-center">Please Wait !</p>
+          <p class="text-center">ভ্রুম ভ্রুম ভ্রুম 😛😛😛</p>
+        </div>
+      </div>
+      <!-- loading ends -->
+
+`
+
